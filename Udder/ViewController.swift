0862ec80fec581d9d5ef3ceafb85f2cb4c050c1e
HugoJaspers <67834661+HugoJaspers@users.noreply.github.com>
2020-08-20 17:48:21 +0200

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by HugoJaspers2 on 20/08/2020.
+//  Copyright © 2020 Hugo Jaspers (personal). All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
